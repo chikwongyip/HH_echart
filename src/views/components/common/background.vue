dc069e5c6b9dd9ba24918ff2aa9eb024b74a7ee8
chikwongyip <527439024@qq.com>
2024-01-05 07:49:21 +0800

    init

---
+++ src/views/components/common/background.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div className="canvas" style="opacity: .2">
+    <iframe frameBorder="0" src="../../../../" style="width: 100%; height: 100%"></iframe>
+  </div>
+</template>
+
+<style scoped lang="less">
+
+</style>
